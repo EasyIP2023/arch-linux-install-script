39cffa2b3c748428e48329c50c4f312d9e7a4b6b
EasyIP2023 <vdavis2495@gmail.com>
2019-04-05 13:40:00 +0000

    update pics and configs

--- README.md
+++ README.md
@@ -7,6 +7,5 @@ git clone htttps://github.com:EasyIP2023/arch-linux-install-script.git
 ```
 
 # What It looks like
-![files](https://github.com/EasyIP2023/arch-linux-install-script/blob/master/pics/files.png)
-![firefox](https://github.com/EasyIP2023/arch-linux-install-script/blob/master/pics/firefox.png)
-![terminals](https://github.com/EasyIP2023/arch-linux-install-script/blob/master/pics/terminals.png)
+![screen term](https://github.com/EasyIP2023/arch-linux-install-script/blob/master/pics/window_screen.png)
+![firefox](https://github.com/EasyIP2023/arch-linux-install-script/blob/master/pics/window_term_fire.png)
--- chroot_install.sh
+++ chroot_install.sh
@@ -195,7 +195,7 @@ install_bootloader () {
   echo "  LINUX ../vmlinuz-linux" >> /boot/syslinux/syslinux.cfg
   echo "  APPEND cryptdevice=/dev/sda2:r00t root=/dev/mapper/r00t rw ipv6.disable=1" >> /boot/syslinux/syslinux.cfg
   echo "  INITRD ../initramfs-linux.img" >> /boot/syslinux/syslinux.cfg
-  sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/g' /etc/mkinitcpio.conf 
+  sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/g' /etc/mkinitcpio.conf
   pacman -S f2fs-tools btrfs-progs --noconfirm
   mkinitcpio -p linux
 
@@ -301,7 +301,7 @@ copy_configs () {
   mv -v .local /home/"${NORMAL_USER}"
 
   mkdir -v /home/"${NORMAL_USER}"/Pictures
-  mv -v neon.jg /home/"${NORMAL_USER}"/Pictures
+  mv -v pics/attack-on-titan.png /home/"${NORMAL_USER}"/Pictures
 
   chown -Rv "${NORMAL_USER}":users /home/"${NORMAL_USER}"/.config
   chown -Rv "${NORMAL_USER}":users /home/"${NORMAL_USER}"/Pictures
